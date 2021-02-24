30e7b7086152d41d843993fccfe5674e2db442fd
Kirstin <31889760+FlareonFan@users.noreply.github.com>
2021-02-24 16:07:53 +0000

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int EnemyHealth = 10;
+
+    void DeductPoints(int DamageAmount)
+    {
+        EnemyHealth -= DamageAmount;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(EnemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
