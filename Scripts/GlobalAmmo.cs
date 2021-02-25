3f898867d99cdfd400db0d702b1749d94565f461
Kirstin <31889760+FlareonFan@users.noreply.github.com>
2021-02-25 09:01:31 +0000

    Add files via upload

---
+++ Scripts/GlobalAmmo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalAmmo : MonoBehaviour
+{
+
+    public static int CurrentAmmo;
+    public int InternalAmmo;
+    public GameObject AmmoDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InternalAmmo = CurrentAmmo;
+        AmmoDisplay.GetComponent<Text>().text = " " + InternalAmmo;
+    }
+}
+
