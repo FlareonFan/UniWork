1178f07765dddc02465aa74dcf05f5b5822d5d56
Kirstin <31889760+FlareonFan@users.noreply.github.com>
2021-02-25 09:44:50 +0000

    Add files via upload

---
+++ Scripts/AmmoPickUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        GlobalAmmo.CurrentAmmo += 10;
+        this.gameObject.SetActive(false);
+    }
+}
