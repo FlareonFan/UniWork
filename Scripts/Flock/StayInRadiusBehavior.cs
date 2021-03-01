e38c9c795e4dcaafb77037bb2803638e40bb80f9
Kirstin <31889760+FlareonFan@users.noreply.github.com>
2021-03-01 21:23:16 +0000

    Add files via upload

---
+++ Scripts/Flock/StayInRadiusBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Stay In Radius")]
+public class StayInRadiusBehavior : FlockBehaviour
+{
+    public Vector2 center;
+    public float radius = 15f;
+
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        Vector2 centerOffset = center - (Vector2)agent.transform.position;
+        float t = centerOffset.magnitude / radius;
+        if (t < 0.9f)
+        {
+            return Vector2.zero;
+        }
+
+        return centerOffset * t * t;
+    }
+}
